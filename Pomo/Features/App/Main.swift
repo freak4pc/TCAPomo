3f603207b4c58ea6ce228b01cbb067effc461f9c
Shai Mishali <freak4pc@gmail.com>
2023-04-17 22:45:17 +0300

    Side-effect with clock

--- Pomo/Features/App/Main.swift
+++ Pomo/Features/App/Main.swift
@@ -17,24 +17,23 @@ struct Main: Reducer {
             switch action {
             case .startTapped:
                 state.isTimerActive = true
-                return .none
-//                return .run { [currentProgress = state.timerProgress] send in
-////                    let neededTicks = Int(Self.timerTicks * (1.0 - currentProgress))
-//                    for _ in stride(from: 1, to: Self.timerTicks, by: 1) {
-//                        try await clock.sleep(for: .seconds(1))
-//                        await send(.timerTicked)
-//                    }
-//
-//                    await send(.stopTapped)
-//                }
+                return .run { send in // [currentProgress = state.timerProgress] send in
+//                    let neededTicks = Int(Self.timerTicks * (1.0 - currentProgress))
+                    for _ in stride(from: 1, to: Self.timerTicks, by: 1) {
+                        try await clock.sleep(for: .seconds(1))
+                        await send(.timerTicked)
+                    }
+
+                    await send(.stopTapped)
+                }
 //                .cancellable(id: TimerCancelID.self)
             case .timerTicked:
                 state.timerProgress += 1 / (25 * 60)
                 return .none
             case .stopTapped:
                 state.isTimerActive = false
-//                state.timerProgress = 0
-//                state.timerTitle = ""
+                state.timerProgress = 0
+                state.timerTitle = ""
 
                 return .none // cancel(id: TimerCancelID.self)
             case .timerTitleChanged(let newTitle):
